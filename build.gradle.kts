056de7e71caa75706d476b66781499ec9687efb9
v-for-vasiliev <vasiliev.k.v@gmail.com>
2022-08-16 09:00:51 +0300

    Add Ktor server

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,17 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    application
     kotlin("jvm") version "1.7.10"
+    kotlin("plugin.serialization") version "1.7.10"
 }
 
 group = "com.fatsnakes"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("com.fatsnakes.splitstupid.MainKt")
+}
 
 repositories {
     mavenCentral()
@@ -25,4 +31,8 @@ tasks.withType<KotlinCompile> {
 
 dependencies {
     implementation("org.javamoney:moneta:1.4.2")
+    implementation("io.ktor:ktor-server-core:2.1.0")
+    implementation("io.ktor:ktor-server-netty:2.1.0")
+    implementation("io.ktor:ktor-server-content-negotiation:2.1.0")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.1.0")
 }
--- src/main/kotlin/com/fatsnakes/splitstupid/Main.kt
+++ src/main/kotlin/com/fatsnakes/splitstupid/Main.kt
@@ -1,12 +1,23 @@
 package com.fatsnakes.splitstupid
 
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 import org.javamoney.moneta.Money
+import java.lang.Exception
 import javax.money.CurrencyUnit
 import javax.money.Monetary
 import javax.money.MonetaryAmount
 import kotlin.random.Random
 
-data class User(
+data class UserModel(
     val name: String,
     val deposit: MonetaryAmount,
     var balance: MonetaryAmount
@@ -15,55 +26,99 @@ data class User(
     fun isBorrower() = balance.isPositive
 }
 
-val rur: CurrencyUnit = Monetary.getCurrency("RUR")
+@Serializable
+data class Participant(
+    val name: String,
+    val amount: Float
+)
 
-val transactions = mutableListOf<String>()
+@Serializable
+data class Transaction(
+    val from: String,
+    val to: String,
+    val amount: MonetaryAmount
+) {
+    override fun toString(): String {
+        return "$from -> $to: $amount"
+    }
+}
+
+val rur: CurrencyUnit = Monetary.getCurrency("RUR")
+val testData = mapOf(
+    "Жека" to 5100.25f,
+    "Макс" to 3550.32f,
+    "Илюха" to 0.0f,
+    "Олег" to 0.0f,
+    "Данич" to 0.0f,
+    "Стасян" to 0.0f,
+    "Кабанич" to 1000.0f,
+    "Поша" to 0.0f,
+    "Кораллыч" to 500.0f
+)
 
 fun main() {
-    val data = mapOf(
-        "Жека" to 5100.25f,
-        "Макс" to 3550.32f,
-        "Илюха" to 0.0f,
-        "Олег" to 0.0f,
-        "Данич" to 0.0f,
-        "Стасян" to 0.0f,
-        "Кабанич" to 1000.0f,
-        "Поша" to 0.0f,
-        "Кораллыч" to 500.0f
-    )
+    embeddedServer(Netty, port = 8080) {
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
+        routing {
+            post("/split") {
+                val participants = call.receiveOrNull<List<Participant>>()
+                participants?.let {
+                    try {
+                        call.respond(split(participants))
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+    }.start(wait = true)
+}
 
-    val total = Money.of(data.values.reduce { acc, p -> acc + p }, rur)
-    val divResult = total.divideAndRemainder(data.size)
+fun split(participants: List<Participant>): List<Transaction> {
+    val transactions = mutableListOf<Transaction>()
+    val total = Money.of(participants.map { it.amount }.reduce { acc, amount -> acc + amount }, rur)
+    val divResult = total.divideAndRemainder(participants.size)
     val average = divResult[0]
     val remainder = divResult[1]
 
-    val users = data.map {
-        val deposit = Money.of(it.value, rur)
+    val users = participants.map {
+        val deposit = Money.of(it.amount, rur)
         val balance = average.subtract(deposit)
-        User(name = it.key, deposit = deposit, balance = balance)
+        UserModel(name = it.name, deposit = deposit, balance = balance)
     }
 
     randomizeRemainderDebt(users, remainder)
 
-    println("--------- Users: ---------")
+    println("------------- Users: ------------")
     users.forEach { println(it) }
 
     val start = System.currentTimeMillis()
     do {
-        users.filter { !it.isSatisfied() }.forEach { satisfyUser(it, users, average) }
+        users.filter { !it.isSatisfied() }.forEach {
+            transactions.addAll(satisfyUser(it, users, average))
+        }
     } while (users.hasUnsatisfied())
     val time = System.currentTimeMillis() - start
 
     println("--------- Transactions: --------- (time: $time ms)")
     transactions.forEach { println(it) }
+
+    return transactions
 }
 
-fun randomizeRemainderDebt(users: List<User>, remainder: MonetaryAmount) {
+fun randomizeRemainderDebt(users: List<UserModel>, remainder: MonetaryAmount) {
     val randomUser = users[Random(System.currentTimeMillis()).nextInt(users.size)]
     randomUser.balance = randomUser.balance.add(remainder)
 }
 
-fun satisfyUser(user: User, users: List<User>, average: MonetaryAmount) {
+fun satisfyUser(user: UserModel, users: List<UserModel>, average: MonetaryAmount): List<Transaction> {
+    val transactions = mutableListOf<Transaction>()
     while (!user.isSatisfied()) {
         var credit = if (user.balance.abs() >= average) average else user.balance.abs()
         var partner = users.firstOrNull { isSuitablePartner(user, it, credit) }
@@ -74,17 +129,18 @@ fun satisfyUser(user: User, users: List<User>, average: MonetaryAmount) {
         if (user.isBorrower()) {
             user.balance = user.balance.subtract(credit)
             partner.balance = partner.balance.add(credit)
-            transactions.add("${user.name} -> ${partner.name}: $credit")
+            transactions.add(Transaction(from = user.name, to = partner.name, amount = credit))
         } else {
             user.balance = user.balance.add(credit)
             partner.balance = partner.balance.subtract(credit)
-            transactions.add("${partner.name} -> ${user.name}: $credit")
+            transactions.add(Transaction(from = partner.name, to = user.name, amount = credit))
         }
     }
+    return transactions
 }
 
-fun isSuitablePartner(user: User, partner: User, credit: MonetaryAmount): Boolean {
+fun isSuitablePartner(user: UserModel, partner: UserModel, credit: MonetaryAmount): Boolean {
     return user.name != partner.name && user.balance.isPositive != partner.balance.isPositive && partner.balance.abs() >= credit
 }
 
-fun List<User>.hasUnsatisfied() = this.firstOrNull { !it.isSatisfied() } != null
+fun List<UserModel>.hasUnsatisfied() = this.firstOrNull { !it.isSatisfied() } != null
